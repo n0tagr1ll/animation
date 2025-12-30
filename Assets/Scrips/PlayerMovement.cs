bee3e28cd7133cc63d4126db80ba8dc6e348760a
n0tagr1ll <a.sariya10@gmail.com>
2025-12-30 19:52:08 +0100

    NEW AND IMPROVED

--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -1,40 +1,41 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem; //her bruger vi unity input system
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    private Animator myAnimator; 
-    private Vector2 movement;
-    private Rigidbody2D myBody;
+    private Vector2 movement; //bevægelse af playeren
+    private Rigidbody2D myBody; 
+    private Animator myAnimator; //animator til playeren
 
-    [SerializeField] private int speed = 5;
 
-    private void Awake()
+    [SerializeField] private int speed = 5; //hastighed på playeren
+
+    private void Awake() //awake funktion som køre når scriptet bliver loadet
     {
-        myBody = GetComponent<Rigidbody2D>();
-        myAnimator = GetComponent<Animator>();
+        myBody = GetComponent<Rigidbody2D>(); //henter rigidbody2d componenten
+        myAnimator = GetComponent<Animator>(); //henter animator componenten
     }
 
-    private void OnMovement(InputValue value)
+    private void OnMovement(InputValue value) //funktion som køre når der bliver givet input til bevægelse
     {
-        movement = value.Get<Vector2>();
+        movement = value.Get<Vector2>(); //sætter movement variablen til den værdi som bliver givet fra input systemet
 
-        if (movement.x != 0 || movement.y != 0)
+        if (movement.x != 0 || movement.y != 0) //tjekker om der er bevægelse
         {
 
-            myAnimator.SetFloat("x", movement.x);
-            myAnimator.SetFloat("y", movement.y);
+            myAnimator.SetFloat("x", movement.x); //sætter animatorens x parameter til movement x værdien
+            myAnimator.SetFloat("y", movement.y); //sætter animatorens y parameter til movement y værdien
 
-            myAnimator.SetBool("IsWalking", true);
+            myAnimator.SetBool("IsWalking", true); //sætter animatorens IsWalking parameter til true
         }
         else
-            myAnimator.SetBool("IsWalking", false);
+            myAnimator.SetBool("IsWalking", false); //ellers sætter den til false
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() //fixed update funktion som køre hver frame
     {
-        myBody.linearVelocity = movement * speed;
+        myBody.linearVelocity = movement * speed; //sætter rigidbody2dens linear velocity til movement værdien gange med speed værdien
     }
 }
